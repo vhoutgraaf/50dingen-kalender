7744c780e8d3c316a9f65ce3f0dbdb6b1b5d0509
houtgvr <vincent.houtgraaf@rigd-loxia.nl>
2016-11-22 16:43:37 +0100

    eerst commit

---
+++ js/kalender.js
@@ -0,0 +1,153 @@
+
+var $root = $('html, body');
+
+var kalenderInstellingen = kalenderDefaultsJSON;
+
+$(document).ready(function(){
+	var instellingen = getUrlParameter('instellingen');
+	if (typeof instellingen !== "undefined") {
+		kalenderInstellingen = JSON.parse(instellingen);
+		alert(JSON.stringify(kalenderInstellingen, null, 2));
+	} else {
+		alert('geen instellingen meegegeven');
+	}
+
+	fillDefaultData();
+	activateSelectStartmaand();
+	activateShowHidePrintInstellingen();
+	activateAnimatedLinkToResultaat();
+	activateSerializeJSONToString();
+
+});
+
+
+
+var fillDefaultData = function() {
+	// vul de maanden in, te beginnen bij maand met index 0: dat is januari.
+	fillInvoerMaandData(0);
+	
+};
+
+// Show of Hide de Instellingen-div door te klikken op de resultaatDiv.
+// Eerst kijken of de Instellingen-div zichtbaar is. Zoja: verbergen. Zonee: laten zien.
+var activateShowHidePrintInstellingen = function() {
+    $("#resultaatDiv").click(function(){
+		
+		if ($("#instellingenDiv").is(":visible")) {
+			$("#instellingenDiv").hide();
+			$("#resultaatTitel").hide();
+			
+			$("#resultaatDiv").attr('class', 'divResultaatZonderInstellingen');
+			
+		} else {
+			$("#instellingenDiv").show();
+			$("#resultaatTitel").show();
+
+			$("#resultaatDiv").attr('class', 'divalgemeen divResultaatSamenMetInstellingen');
+		}
+    });
+};
+
+// activeer de selectbox om een startmaand te kunnen selecteren.
+var activateSelectStartmaand = function() {
+	var selectedIndex = -1;
+	$( "#startmaandSelect" )
+		  .change(function() {
+			var str = "";
+			$( "select option:selected" ).each(function() {
+			  str += $( this ).text() + " ";
+			  selectedIndex = $( this ).val();
+			});
+			$( "#debugDiv" ).text( str );
+			fillInvoerMaandData(selectedIndex*1);
+		  })
+		  .trigger( "change" );
+
+};
+
+// vul de invoerteksten voor de maanden in, te beginnen bij de geselecteerde maand
+var fillInvoerMaandData = function(selectedMaandIndex) {
+	for (var i=1; i<=12; i++) {
+		$("#invoerMaandTitel"+i).val(kalenderInstellingen.maanden[(i+selectedMaandIndex)<=12 ? i+selectedMaandIndex-1 : (i+selectedMaandIndex-1)%12].titel);
+		$("#invoerMaandTekst"+i).val(kalenderInstellingen.maanden[(i+selectedMaandIndex)<=12 ? i+selectedMaandIndex-1 : (i+selectedMaandIndex-1)%12].tekst);
+	}
+};
+
+// vul de resultaatteksten voor de maanden in, te beginnen bij de geselecteerde maand
+var fillResultaatMaandData = function() {
+	for (var i=1; i<=12; i++) {
+		$("#resultaatMaandTitel"+i).text($("#invoerMaandTitel"+i).val());
+		$("#resultaatMaandTekst"+i).text($("#invoerMaandTekst"+i).val());
+	}
+};
+
+
+// scroll langzaam naar het anchor '#anchorBijResultaat'
+// de # hoort hier niet bij jQuery om een HTML-taq met een bepaald id op te zoeken maar hoort bij aan anchor in de tekst. 
+// Een anchor is een plek in je pagina waarnaar je toe kan linken. Probeer maar eens file:///K:/Projecten/kalender-50-dingen/kalender.html#anchorBijResultaat
+var activateAnimatedLinkToResultaat = function () {
+	$('#linkNaarResultaat').click(function(){
+		fillResultaatMaandData();
+		$root.animate({
+			scrollTop: $( $(this).attr('href') ).offset().top
+		}, 'slow');
+	});
+};
+
+// dit is nodig om de instellingen op te kunnen slaan
+var maakJSONVanInvoer = function() {
+	var url = 'file:///K:/Projecten/kalender-50-dingen/kalender.html';
+	var json = '{"maanden": [';
+	var elBegin = '{';
+	var elEind = '}';
+	var elQuote = '"';
+	var elLijm = ',';
+	for (var i=1; i<=12; i++) {
+		json += elBegin + elQuote + 'titel' + elQuote + ':' + elQuote + $("#invoerMaandTitel"+i).val() + elQuote;
+		json += elLijm;
+		json += elQuote + 'tekst' + elQuote + ':' + elQuote + $("#invoerMaandTekst"+i).val() + elQuote + elEind;
+		if (i!=12) {
+			json += ',';
+		}
+	}
+	json += ']}';
+	return url + '?instellingen=' + encodeURIComponent(json);
+}
+
+
+
+// maak een url die je kan opslaan
+var serializeJSONToString = function() {
+	var json_text = JSON.stringify(kalenderInstellingen, null, 2);
+	json_text = maakJSONVanInvoer();
+	return json_text;
+};
+
+var activateSerializeJSONToString = function() {
+		$("#serializeJSON").click(function(){
+			var urlVoorOpslag = serializeJSONToString();
+			$('#linkVoorOpslag').attr('href',urlVoorOpslag);
+			$("#linkVoorOpslag").attr('class', 'linkVoorOpslagZichtbaar');
+
+//			alert(serializeJSONToString());
+    });
+
+};
+
+
+// Zie: http://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
---
+++ js/kalenderdata.js
@@ -0,0 +1,41 @@
+
+var kalenderDefaultsJSON = {
+	"maanden": [{
+		"titel": "Januari",
+		"tekst": "Dit is de eerste maand van het jaar"
+	}, {
+		"titel": "Februari",
+		"tekst": "Dit is de tweede maand van het jaar"
+	}, {
+		"titel": "Maart",
+		"tekst": "Dit is de derde maand van het jaar"
+	}, {
+		"titel": "April",
+		"tekst": "Dit is de vierde maand van het jaar"
+	}, {
+		"titel": "Mei",
+		"tekst": "Dit is de vijfde maand van het jaar"
+	}, {
+		"titel": "Juni",
+		"tekst": "Dit is de zesde maand van het jaar"
+	}, {
+		"titel": "Juli",
+		"tekst": "Dit is de zevende maand van het jaar"
+	}, {
+		"titel": "Augustus",
+		"tekst": "Dit is de achtste maand van het jaar"
+	}, {
+		"titel": "September",
+		"tekst": "Dit is de negende maand van het jaar"
+	}, {
+		"titel": "Oktober",
+		"tekst": "Dit is de tiende maand van het jaar"
+	}, {
+		"titel": "November",
+		"tekst": "Dit is de elfde maand van het jaar"
+	}, {
+		"titel": "December",
+		"tekst": "Dit is de twaalfde maand van het jaar"
+	}]
+};
+
